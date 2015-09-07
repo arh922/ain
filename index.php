e064fa2825afd6fd63895eb0cbe3cfa848475a04
Ayman Hussein <arh922@hotmail.com>
2015-09-07 12:26:26 +0300

    add git to index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 //echo('111111111111111');
-include("includes/header.php");
+include("includes/header.php");//git
 ?>
 <div id="new-task" class="modal hide" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-header">
